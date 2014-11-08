3660bc6a0088b18a27383565342a3483e5e01a7c
Ethan Jucovy <ethan.jucovy@gmail.com>
2014-11-08 18:21:44 -0500

    dotenv

--- manage.py
+++ manage.py
@@ -8,6 +8,9 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
 
+import dotenv
+dotenv.read_dotenv()
+
 import settings
 
 if __name__ == "__main__":
